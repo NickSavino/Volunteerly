233e704b36673244f6953ab5bd07274fe5fc944e
Nick SAVINO <nicola.savino1@ucalgary.ca>
2026-03-06 06:46:18 -0700

    Setup local supabase environment and auth middleware

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express"
+import { supabase } from "../lib/supabase.js";
+import { env } from "../lib/env.js";
+
+type AuthenticatedRequest = Request & {
+    auth?: {
+        userId: string;
+        email: string | undefined;
+    };
+};
+
+export async function auth(
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader?.startsWith("Bearer ")) {
+        return res.status(401).json({
+            error: "Unauthorized",
+            message: "Missing or invalid bearer token."
+        });
+    }
+
+    const token = authHeader.replace("Bearer ", "").trim();
+
+    const { data, error } = await supabase.auth.getUser(token);
+
+    if (error || !data.user) {
+        return res.status(401).json({
+            error: "Unauthorized",
+            message: "Invalid or expired token."
+        })
+    }
+
+    req.auth = {
+        userId: data.user.id,
+        email: data.user.email
+    };
+
+    next();
+}
