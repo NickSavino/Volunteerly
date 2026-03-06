db355583230a68da4d934f3ff745281e850488c2
Nick SAVINO <nicola.savino1@ucalgary.ca>
2026-03-06 06:54:09 -0700

    infra:  general tweaks, updated docker-compose-make files, dependencies. setup env.ts for server

--- server/src/lib/env.ts
+++ server/src/lib/env.ts
@@ -6,6 +6,8 @@ const envSchema = z.object({
     PORT: z.coerce.number(),
     CORS_ORIGIN: z.string(),
     DATABASE_URL: z.string().min(1),
+    SUPABASE_URL: z.string().min(1),
+    SUPABASE_SERVICE_ROLE_KEY: z.string()
 })
 
 export const env = envSchema.parse(process.env);
