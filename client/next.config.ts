307ef8a0925baeeb176632f0ed2352e4bab476bc
Nick SAVINO <nicola.savino1@ucalgary.ca>
2026-03-07 01:45:06 -0700

    infra: Initalize UI foundation and client config - disabled turbopack to enable hot reload in docker container. - updated dockerfile to run client container mount directly in its workspace - Updated next config - updated package.json (disable turbopack) - Initialized shacdn/ui and added base components

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@volunteerly/shared"]
+  transpilePackages: ["@volunteerly/shared"],
+  watchOptions: {
+    pollIntervalMs: 800,
+  },
 };
 
 export default nextConfig;
