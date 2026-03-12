d457c452ee3fd44f13fbe2ddb2516d659994003f
mkrn52 <mankaranpreet.pandhe@ucalgary.ca>
2026-03-12 01:26:41 -0600

    update users table to contain name

--- shared/src/schemas/user.ts
+++ shared/src/schemas/user.ts
@@ -6,6 +6,8 @@ export type UserRole = z.infer<typeof UserRoleSchema>;
 export const UserSchema = z.object({
     id: z.uuid(),
     email: z.email(),
+    fName: z.string(),
+    lName: z.string(),
     role: UserRoleSchema,
     createdAt: z.iso.datetime(),
     updatedAt: z.iso.datetime(),
@@ -15,6 +17,8 @@ export type User = z.infer<typeof UserSchema>;
 export const CurrentUserSchema = z.object({
     id: z.uuid(),
     email: z.email(),
+    fName: z.string(),
+    lName: z.string(),
     role: UserRoleSchema,
     createdAt: z.iso.datetime(),
     updatedAt: z.iso.datetime(), 
@@ -25,6 +29,8 @@ export type CurrentUser = z.infer<typeof CurrentUserSchema>
 export const CurrentUserUpdateSchema = z.object({
     id: z.uuid(),
     email: z.email(),
+    fName: z.string(),
+    lName: z.string(),
     role: UserRoleSchema,
     createdAt: z.iso.datetime(),
     updatedAt: z.iso.datetime(), 
