80970a950793cb0d4f8611e17167cb4af6214704
Mankaranpreet Pandher <mankaranpreet.pandhe@ucalgary.ca>
2026-03-14 15:40:25 -0600

    feat: implement general home page, 2-column page with charity image and instrucitons

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,6 +6,15 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "../providers/auth-provider";
 import { useEffect } from "react";
 import { Navbar } from "@/components/custom/login_navbar";
+import tms from "@/assets/tms.png"
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item"
+import { InboxIcon, User, Upload, Merge, HandHeart, HouseHeart } from "lucide-react";
 
 export default function LandingPage() {
   const router = useRouter();
@@ -21,28 +30,113 @@ export default function LandingPage() {
     return <main className="p-6">Loading...</main>
   }
   return (
-    <div className="min-h-screen px-6">
+    <div className="min-h-screen">
       <Navbar></Navbar>
-      <main className="flex min-h-screen flex-col items-center justify-between px-6">
-        <div className="w-full max-w-2x1 text-center space-y-6">
-          <div className="space-y-2">
-            <h1 className="text-4x1 font-bold tracking-tight">Volunteerly</h1>
-            <p className="text-muted-foreground text-lg">
-              Turn skills into real impact.
+      <main className="flex flex-1 h-[calc(100vh-64px)]">
+        <div className="w-1/2 relative h-full overflow-hidden">
+          <img
+            src={tms.src}
+            alt="Preview"
+            className="w-full h-full"
+          />
+          <div className="absolute inset-0 w-full bg-black/50"></div>
+          <div className="absolute bottom-20 left-12 text-left">
+            <h1 className="text-4x1 text-muted font-bold tracking-tight pb-2">
+              Turn Skills Into Real Impact
+            </h1>
+            <h3 className="text-2x1 text-muted font-bold tracking-tight pb-5">
+              AI-Powered Matching for Skilled Volunteering
+            </h3>
+            <p className="text-muted text-lg">
+              "Through Volunteerly, we were able to find volunteers for our most complex tasks, allowing us to devote more funds to hepling our cause."
             </p>
           </div>
-
-          <div className="flex items-center justify-center gap-4">
-            <Button asChild>
-              <Link href="/login">Log in</Link>
-            </Button>
-
-            <Button asChild variant="outline">
-              <Link href="/signup">Sign Up</Link>
-            </Button>
-          </div>
         </div>
-      </main>
+        <div className="w-1/2 items-center h-full flex flex-col px-8 ">
+          <h1 className="text-4x1 font-bold tracking-tight pt-5 text-left">How It Works?</h1>
+          <div className="flex flex-col flex-1 justify-around">
+            <div className="space-y-0 text-left">
+              <h2 className="text-xl font-semibold text-muted-foreground mb-2 flex items-center gap-2"> 
+                <User className="inline-block" /> 
+                For Volunteers
+              </h2>
+              <Item>
+                <ItemMedia variant="icon">
+                  <Upload />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>UPLOAD EXPERIENCE</ItemTitle>
+                  <ItemDescription>
+                    Share your professional background to find the perfect match.
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+              <Item>
+                <ItemMedia variant="icon">
+                  <Merge />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>MATCH WITH OPPORTUNITIES</ItemTitle>
+                  <ItemDescription>
+                    Browse curated high-impact projects that align with your passion.
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+              <Item>
+                <ItemMedia variant="icon">
+                  <HandHeart />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>VOLUNTEER YOUR SKILLS</ItemTitle>
+                  <ItemDescription>
+                    Collaborate with non-profits and drive real social change.
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+            </div>
+            <div className="space-y-0 text-left">
+              <h2 className="text-xl font-semibold text-muted-foreground mb-2 flex items-center gap-2"> 
+                <HouseHeart className="inline-block" /> 
+                For Organizations
+              </h2>
+              <Item>
+                <ItemMedia variant="icon">
+                  <Upload />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>POST AN OPPURTUNITY</ItemTitle>
+                  <ItemDescription>
+                    Define your needs and create a  listing for expert help.
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+              <Item>
+                <ItemMedia variant="icon">
+                  <Merge />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>BROWSE SKILLED TALENT</ItemTitle>
+                  <ItemDescription>
+                    Review profiles of vetted professionals ready to contribute.
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+              <Item>
+                <ItemMedia variant="icon">
+                  <HandHeart />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>GROW YOUR IMPACT</ItemTitle>
+                  <ItemDescription>
+                    Successfully complete projects and scale your organization's reach.
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+            </div>
+          </div>
+
+        </div>
+        </main>
     </div>
   );
 }
