60dd25513e01d9de513627faf00b8f1b17d6ff6b
Saad Abdullah <saad.abdullah1@ucalgary.ca>
2026-03-16 17:16:11 -0600

    fix: update index.ts to include new moderator router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { healthRouter } from "./health.js";
 import { currentUserRouter } from "./current-user.js";
-import { moderatorRouter } from "./moderator.js";
 import { currentVolunteerRouter } from "./current-volunteer.js";
 import { currentOrganizationRouter } from "./current-organization.js";
 import { OrganizationRouter } from "./organization.js";
+import { currentModeratorRouter } from "./current-moderator.js";
 
 /**
  * Base Router for all routes. Injected into app.ts
@@ -16,7 +16,7 @@ export const apiRouter = Router();
 
 apiRouter.use("/health", healthRouter);
 apiRouter.use("/current-user", currentUserRouter);
-apiRouter.use("/moderator", moderatorRouter);
 apiRouter.use("/current-volunteer", currentVolunteerRouter);
 apiRouter.use("/current-organization", currentOrganizationRouter);
 apiRouter.use("/organization", OrganizationRouter);
+apiRouter.use("/current-moderator", currentModeratorRouter);
