2045e65b8283027761d3f487558af8bf3dab88c9
Saad Abdullah <saad.abdullah1@ucalgary.ca>
2026-03-16 17:06:27 -0600

    fix: updated index.ts

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -3,8 +3,7 @@ export * from "./schemas/api.js";
 export * from "./schemas/user.js";
 
 export * from "./schemas/moderator.js";
-export * from "./schemas/user";
 
-export * from "./schemas/volunteer";
+export * from "./schemas/volunteer.js";
 
-export * from "./schemas/organization";
+export * from "./schemas/organization.js";
