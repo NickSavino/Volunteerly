84ca6f7f4cf165c05329192ff3f1ab1bdd343678
Saad Abdullah <saad.abdullah1@ucalgary.ca>
2026-03-16 21:06:40 -0600

    feat: added mod methods to org service

--- client/src/services/OrganizationService.tsx
+++ client/src/services/OrganizationService.tsx
@@ -1,36 +1,59 @@
-import { supabase } from "@/lib/supabase";
 import { api } from "@/lib/api";
-import { CurrentOrganizationSchema, CurrentOrganization, CurrentOrganizationUpdateSchema } from "@volunteerly/shared";
-
+import {
+    CurrentOrganizationSchema,
+    CurrentOrganizationUpdateSchema,
+    OrganizationSchema,
+    OrganizationsSchema,
+} from "@volunteerly/shared";
 
 export class OrganizationService {
 
     static async getCurrentOrganization() {
         const response = await api<unknown>("/current-organization");
-        const parsed = CurrentOrganizationSchema.safeParse(response)
-
-        return parsed
+        const parsed = CurrentOrganizationSchema.safeParse(response);
+        return parsed;
     }
 
     static async update_create_Organization(user: CurrentOrganizationUpdateSchema) {
         const response = await api<unknown>("/current-organization", {
-            method:"PUT",
-            body: JSON.stringify(user)
+            method: "PUT",
+            body: JSON.stringify(user),
         });
-        const parsed = CurrentOrganizationSchema.safeParse(response)
-
-        return parsed
+        const parsed = CurrentOrganizationSchema.safeParse(response);
+        return parsed;
     }
 
     static async apply(user: CurrentOrganizationUpdateSchema) {
         const response = await api<unknown>("/current-organization/apply", {
-            method:"PUT",
-            body: JSON.stringify(user)
+            method: "PUT",
+            body: JSON.stringify(user),
         });
-        const parsed = CurrentOrganizationSchema.safeParse(response)
-
-        return parsed
+        const parsed = CurrentOrganizationSchema.safeParse(response);
+        return parsed;
     }
 
+    static async getAllOrganizations(status?: "APPLIED") {
+        const url = status ? `/organization?status=${status}` : "/organization";
+        const response = await api<unknown>(url);
+        const parsed = OrganizationsSchema.safeParse(response);
+        return parsed;
+    }
 
+    static async approveOrganization(orgId: string) {
+        const response = await api<unknown>("/organization/approve", {
+            method: "PUT",
+            body: JSON.stringify({ orgId }),
+        });
+        const parsed = OrganizationSchema.safeParse(response);
+        return parsed;
+    }
+
+    static async rejectOrganization(orgId: string, rejectionReason: string) {
+        const response = await api<unknown>("/organization/reject", {
+            method: "PUT",
+            body: JSON.stringify({ orgId, rejectionReason }),
+        });
+        const parsed = OrganizationSchema.safeParse(response);
+        return parsed;
+    }
 }
