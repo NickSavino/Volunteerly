1860d10ffe1ac598ba04be9fe16fa1f9ebf6a042
Mankaranpreet Pandher <mankaranpreet.pandhe@ucalgary.ca>
2026-03-28 13:52:03 -0600

    feat: created opportunities overview page with tabs for all 3 states, added VM to pull all opps and filter on UI tab

---
+++ client/src/app/organization/opportunities/orgOpportunitiesVm.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
+import { UserService } from "@/services/UserService";
+import { useAuth } from "@/providers/auth-provider";
+import { CurrentOrganization, CurrentUser, CurrentUserSchema, Opportunity } from "@volunteerly/shared";
+import { api } from "@/lib/api";
+import { OrganizationService } from "@/services/OrganizationService";
+
+export function useOrgOpportunitiesViewModel() {
+  const router = useRouter();
+  const { session, user, loading, signOut } = useAuth();
+  const [currentUser, setCurrentUser] = useState<CurrentOrganization | undefined>(undefined);
+  const [error, setError] = useState<string | null>(null);
+  const [opportunities, setOpportunities] = useState<Opportunity[]>([])
+  const [currentTab, setCurrentTab] = useState("OPEN")
+  const filteredOpportunities = opportunities.filter(
+  (opp) => opp.status === currentTab);
+
+  useEffect(() => {
+    if (!loading && !session) {
+      router.replace("/login")
+    }
+  }, [loading, session, router]);
+
+  useEffect(() => {
+    async function loadCurrentUser() {
+      if (!session?.access_token) return;
+      try {
+        const user = await UserService.getCurrentUser()
+
+        if (!user.success) {
+            console.error(user.error);
+            setError("Received invalid user data from the server.");
+            return;
+        }
+
+        if (user.data.role !== "ORGANIZATION") {
+            router.replace("/bootstrap");
+            return;
+        }
+
+        const org = await OrganizationService.getCurrentOrganization()
+        
+        if (!org.success) {
+          console.error(org.error);
+          setError("Received invalid user data from the server.");
+          return;
+        }
+        if (org.data.status == "CREATED") {
+            router.replace("/organization/application");
+            return;
+        } else if (org.data.status == "APPLIED") {
+            router.replace("/organization/appliedDashboard");
+            return;
+        }
+        setCurrentUser(org.data);
+      } catch (error) {
+        console.error(error);
+        return
+      }
+    }
+    loadCurrentUser();
+    }, [session, router]);
+
+    useEffect(() => {
+      async function loadOpportunities() {
+
+        const opps = await OrganizationService.getAllOpportunities()
+        if (!opps.success) { 
+          console.error(opps.error)
+          setError("Failed to load opportunities."); return; }
+        setOpportunities(opps.data);   
+        
+      }
+      loadOpportunities()
+    }, [])
+
+    return {loading, session, signOut, router, user, error, currentUser, filteredOpportunities, currentTab, setCurrentTab} 
+}
