7caa21c3fbf7e8d53878ae5b2c6f0380e716a468
AJStOmer <aaron.stomer@ucalgary.ca>
2026-03-31 03:13:42 -0600

    fix: changed size of vector to match gemini model 001

--- server/prisma/migrations/20260331061941_add_skill_vector_to_volunteer/migration.sql
+++ server/prisma/migrations/20260331061941_add_skill_vector_to_volunteer/migration.sql
@@ -1,4 +1,4 @@
 create extension if not exists vector;
 
 -- AlterTable
-ALTER TABLE "volunteers" ADD COLUMN     "skill_vector" vector(768);
+ALTER TABLE "volunteers" ADD COLUMN     "skill_vector" vector(3072);
