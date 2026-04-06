f27a3ce48a0acb4f829d978721cd541c7b91dbc2
Saad Abdullah <saad.abdullah1@ucalgary.ca>
2026-04-05 22:54:29 -0600

    fix: fixed auth provider code

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -36,7 +36,11 @@ export default function RootLayout({
         />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <AppSessionProvider>
+            {children}
+          </AppSessionProvider>
+        </AuthProvider>
         <Toaster position="bottom-right" expand={false} richColors={true} />
       </body>
     </html>
