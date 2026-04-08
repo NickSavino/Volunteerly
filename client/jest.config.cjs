8f2a304aede41eb4fa5363e71c25519fb253aafd
Nick SAVINO <nicola.savino1@ucalgary.ca>
2026-04-08 03:46:23 -0600

    feat: setup jest installation

---
+++ client/jest.config.cjs
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require("next/jest")
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig = {
+  roots: ["<rootDir>/test"],
+  testEnvironment: "jsdom",
+  setupFiles: ["<rootDir>/test/jest.env.ts"],
+  setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@volunteerly/shared$": "<rootDir>/../shared/src/index.ts",
+    "^@volunteerly/shared/(.*)$": "<rootDir>/../shared/src/$1",
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
